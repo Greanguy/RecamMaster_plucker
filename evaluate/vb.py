6812576b1f9b01790f8a59b3e2d6faf09d2d4dda
L-kj12138 <3152545742@qq.com>
2025-11-15 10:32:42 +0800

    evaluate

--- evaluate/vb.py
+++ evaluate/vb.py
@@ -1,8 +1,8 @@
 from vbench import VBench
 
 device = "cuda:0"  
-full_info_json = "vbench/VBench_full_info.json"  # 只有在使用提示集生成的视频的时候需要用,即在mode为vbench_standard或者vbench_category的时候
-save_dir = "/data1/home/liu_kai/RecamMaster_plucker/evaluate/vbench"                 
+full_info_json = "vbench/VBench_full_info.json"
+save_dir = "/data1/home/liu_kai/RecamMaster_plucker/evaluate/vbench/plucker"                 
 
 my_VBench = VBench(
     device=device,
@@ -10,17 +10,25 @@ my_VBench = VBench(
     output_path=save_dir,
 )
 
-my_VBench.evaluate(
-    videos_path="/data1/home/liu_kai/ReCamMaster/recam_result/cam_type3",  
-    name="cam03_eval",       # 这会决定输出文件名前缀
-    dimension_list=[
-        "subject_consistency",
-        "background_consistency",
-        "motion_smoothness",
-        "dynamic_degree",
-        "aesthetic_quality",
-        "imaging_quality",
-        "temporal_flickering",
-    ],
-    mode="custom_input",               
-)
+cam_types = ["1", "3", "5", "7", "9"]
+
+for cam_type in cam_types:
+    videos_path = f"/data1/home/liu_kai/ReCamMaster/ckpt_20000_plucker/cam_type{cam_type}"
+    name = f"cam{cam_type}_eval"
+    
+    print(f"eval-ing: {name}")
+    
+    my_VBench.evaluate(
+        videos_path=videos_path,
+        name=name,
+        dimension_list=[
+            "subject_consistency",
+            "background_consistency",
+            "motion_smoothness", 
+            "dynamic_degree",
+            "aesthetic_quality",
+            "imaging_quality",
+            "temporal_flickering",
+        ],
+        mode="custom_input",
+    )
--- evaluate/vipe/cam_pose.sh
+++ evaluate/vipe/cam_pose.sh
@@ -1,17 +1,18 @@
 # vipe infer .mp4 --output 
 # 处理单个视频或目录（streams.base_path 可以是文件或文件夹）    # streams.base_path=YOUR_VIDEO_OR_DIR_PATH \
 
-CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
-    streams.base_path=/data1/home/liu_kai/ReCamMaster/recam_result/cam_type3 \
-    pipeline.post.depth_align_model=null \
-    pipeline.output.save_viz=false pipeline.slam.visualize=false \
-    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam03 \
-    pipeline.output.save_artifacts=true
+# CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+#     streams.base_path=/data1/home/liu_kai/ReCamMaster/recam_result/cam_type3 \
+#     pipeline.post.depth_align_model=null \
+#     pipeline.output.save_viz=false pipeline.slam.visualize=false \
+#     pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam03 \
+#     pipeline.output.save_artifacts=true
 # 其提取的外参是c2w
 
 # python process_video.py ./vipe_res_cam01/pose/video0.npz cam01_v0.json --cam cam01
-# 批量处理.npz文件并在各自所在目录下生成同名 json
-python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam03/pose --cam cam03 # --prefix frame --inv  
+# 批量处理.npz文件并在各自所在目录下生成同名 json 如果想要把所有 json 输出到指定目录 out_dir（脚本会自动创建）：
+# python process_video.py ./vipe_res_cam01/pose out_dir --cam cam01 
+# python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam03/pose --cam cam03 # --prefix frame --inv  
 # 用以处理vipe提取出的相机外参文件.npz(生成视频都是81帧的,所以vipe提取出的相机外侧也默认是81帧)
 # 参数--cam指定输出.json文件的前缀名; vipe提取的外参是c2w矩阵, 想得到w2c矩阵可在运行process_video.py是加--inv参数, 取个逆即可
 
@@ -21,7 +22,8 @@ python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vi
 #     pipeline.output.save_viz=false pipeline.slam.visualize=false \
 #     pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam01 \
 #     pipeline.output.save_artifacts=true
-# python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam01/pose --cam cam01
+# python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam01/pose \
+#     /data1/home/liu_kai/RecamMaster_plucker/evaluate/cam_err/cam01 --cam cam01
 
 # CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
 #     streams.base_path=/data1/home/liu_kai/ReCamMaster/recam_result/cam_type5 \
@@ -29,12 +31,59 @@ python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vi
 #     pipeline.output.save_viz=false pipeline.slam.visualize=false \
 #     pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam05 \
 #     pipeline.output.save_artifacts=true
-# python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam05/pose --cam cam05
+# python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam05/pose \
+#     /data1/home/liu_kai/RecamMaster_plucker/evaluate/cam_err/cam05 --cam cam05
 
-# CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
-#     streams.base_path=/data1/home/liu_kai/ReCamMaster/recam_result/cam_type7 \
-#     pipeline.post.depth_align_model=null \
-#     pipeline.output.save_viz=false pipeline.slam.visualize=false \
-#     pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam07 \
-#     pipeline.output.save_artifacts=true
-# python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam07/pose --cam cam07
+CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+    streams.base_path=/data1/home/liu_kai/ReCamMaster/recam_result/cam_type7 \
+    pipeline.post.depth_align_model=null \
+    pipeline.output.save_viz=false pipeline.slam.visualize=false \
+    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam07 \
+    pipeline.output.save_artifacts=true
+python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/vipe_res_cam07/pose \
+    /data1/home/liu_kai/RecamMaster_plucker/evaluate/cam_err/cam07 --cam cam07
+
+CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+    streams.base_path=/data1/home/liu_kai/ReCamMaster/ckpt_20000_plucker/cam_type7 \
+    pipeline.post.depth_align_model=null \
+    pipeline.output.save_viz=false pipeline.slam.visualize=false \
+    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam07 \
+    pipeline.output.save_artifacts=true
+python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam07/pose \
+    /data1/home/liu_kai/RecamMaster_plucker/evaluate/plucker/cam_err/cam07 --cam cam07
+
+CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+    streams.base_path=/data1/home/liu_kai/ReCamMaster/ckpt_20000_plucker/cam_type9 \
+    pipeline.post.depth_align_model=null \
+    pipeline.output.save_viz=false pipeline.slam.visualize=false \
+    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam09 \
+    pipeline.output.save_artifacts=true
+python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam09/pose \
+    /data1/home/liu_kai/RecamMaster_plucker/evaluate/plucker/cam_err/cam09 --cam cam09
+
+CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+    streams.base_path=/data1/home/liu_kai/ReCamMaster/ckpt_20000_plucker/cam_type1 \
+    pipeline.post.depth_align_model=null \
+    pipeline.output.save_viz=false pipeline.slam.visualize=false \
+    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam01 \
+    pipeline.output.save_artifacts=true
+python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam01/pose \
+    /data1/home/liu_kai/RecamMaster_plucker/evaluate/plucker/cam_err/cam01 --cam cam01
+
+CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+    streams.base_path=/data1/home/liu_kai/ReCamMaster/ckpt_20000_plucker/cam_type3 \
+    pipeline.post.depth_align_model=null \
+    pipeline.output.save_viz=false pipeline.slam.visualize=false \
+    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam03 \
+    pipeline.output.save_artifacts=true
+python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam03/pose \
+    /data1/home/liu_kai/RecamMaster_plucker/evaluate/plucker/cam_err/cam03 --cam cam03
+
+CUDA_VISIBLE_DEVICES=0 python run.py pipeline=default streams=raw_mp4_stream \
+    streams.base_path=/data1/home/liu_kai/ReCamMaster/ckpt_20000_plucker/cam_type5 \
+    pipeline.post.depth_align_model=null \
+    pipeline.output.save_viz=false pipeline.slam.visualize=false \
+    pipeline.output.path=/data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam05 \
+    pipeline.output.save_artifacts=true
+python process_video.py /data1/home/liu_kai/RecamMaster_plucker/evaluate/vipe/plucker_res_cam05/pose \
+    /data1/home/liu_kai/RecamMaster_plucker/evaluate/plucker/cam_err/cam05 --cam cam05
